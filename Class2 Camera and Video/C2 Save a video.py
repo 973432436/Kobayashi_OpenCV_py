c4cd43b6bba3d8e2141f41fe42de0151b8ced678
Kobayashi <skdliugc@163.com>
2018-01-07 13:43:39 +0800

    .

--- Class2 Camera and Video/C2 Save a video.py
+++ Class2 Camera and Video/C2 Save a video.py
@@ -11,9 +11,9 @@ out = cv2.VideoWriter('VideoDemo.avi', fourcc, 20.0, (640,480))
 while cap.isOpened():
     ret, frame = cap.read()
     if ret is True:
-#		将当前帧写入输出对象
+#       将当前帧写入输出对象
         out.write(frame)
-#		显示当前帧
+#       显示当前帧
         cv2.imshow('Frame', frame)
         if cv2.waitKey(1) & 0xFF is ord('q'):
             break
