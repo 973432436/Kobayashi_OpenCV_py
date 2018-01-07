e6ade09ecb22f44a65d12a298a376372556c1576
Kobayashi <skdliugc@163.com>
2018-01-07 13:41:12 +0800

    .

--- Class2 Camera and Video/C2 Save a video.py
+++ Class2 Camera and Video/C2 Save a video.py
@@ -2,19 +2,25 @@ import numpy as np
 import cv2
 
 cap = cv2.VideoCapture(0)
+
+# 设置编码格式
 fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+# 定义输出对象及输出格式
 out = cv2.VideoWriter('VideoDemo.avi', fourcc, 20.0, (640,480))
 
 while cap.isOpened():
     ret, frame = cap.read()
     if ret is True:
+#		将当前帧写入输出对象
         out.write(frame)
+#		显示当前帧
         cv2.imshow('Frame', frame)
         if cv2.waitKey(1) & 0xFF is ord('q'):
             break
     else:
         break
 
+# 释放资源
 cap.release()
 out.release()
 cv2.destroyAllWindows()
