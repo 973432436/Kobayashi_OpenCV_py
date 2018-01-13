f12f81aed71b92be6679d4acfb16f9a264aaeba0
Kobayashi <skdliugc@163.com>
2018-01-13 20:13:37 +0800

    Update C1 Show Video.py

--- Class 1 Hello World/C1 Show Video.py
+++ Class 1 Hello World/C1 Show Video.py
@@ -8,14 +8,14 @@ cap = cv2.VideoCapture(0)
 while True:
 	if cap.isOpened():
 #       捕获当前帧
-		ret,frame = cap.read()
+        ret,frame = cap.read()
 #       显示图像
-		cv2.imshow('Camera', frame)
+        cv2.imshow('Camera', frame)
 
 #   结束帧捕获的条件
 #   等待100ms 即帧频为10fps
 	if cv2.waitKey(100) & 0xFF is ord('q'):
-		break
+        break
 
 # 释放资源
 cap.release()
