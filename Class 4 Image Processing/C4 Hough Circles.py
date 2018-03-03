cf21f4e6dc4eda8e69a249726b34c71c3fd232b3
Kobayashi <skdliugc@163.com>
2018-03-03 16:29:39 +0800

    .

---
+++ Class 4 Image Processing/C4 Hough Circles.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('E:\\OpenCV\\py_version\\demo\\venv\\Picture\\logo2.jpg')
+grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+circle = cv2.HoughCircles(grayImg, cv2.HOUGH_GRADIENT, 1, 50)
+
+for c in circle:
+	for x,y,r in c:
+		cv2.circle(img, (x,y), r, (0, 0, 0), 2)
+
+cv2.imshow('Circle', img)
+
+cv2.waitKey()
+cv2.destroyAllWindows()
