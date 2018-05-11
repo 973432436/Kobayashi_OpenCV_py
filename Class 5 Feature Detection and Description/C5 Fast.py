2989aaa9855db12715097476d4951bf957d704cb
Kobayashi <skdliugc@163.com>
2018-05-11 13:59:47 +0800

    .

---
+++ Class 5 Feature Detection and Description/C5 Fast.py
@@ -0,0 +1,29 @@
+import numpy as np
+import cv2
+
+img = cv2.imread('Mavic Air Fly.jpeg')
+grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+fast = cv2.FastFeatureDetector_create(threshold=35)
+
+kp = fast.detect(grayImg, None)
+img2 = cv2.drawKeypoints(img, kp, None, (255, 0, 0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+print('Threshold: ', fast.getThreshold())
+print('nonmaxSuppression: ', fast.getNonmaxSuppression())
+print('neighborhood: ', fast.getType())
+print('Total Keypoints with nonmaxSuppression: ', len(kp))
+
+cv2.imshow('fast_true', img2)
+
+fast.setNonmaxSuppression(False)
+kp = fast.detect(grayImg, None)
+
+print('Total Keypoints without nonmaxSuppression: ', len(kp))
+
+img3 = cv2.drawKeypoints(img, kp, None, (255, 0, 0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+cv2.imshow('fast_false', img3)
+
+cv2.waitKey()
+cv2.destroyAllWindows()
